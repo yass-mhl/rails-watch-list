407b4810ef6f6cebb015cc8d232e84ff607d9546
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-02-18 11:20:57 +0100

    add index to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists, only: %i[index new create show destroy] do
+  resources :lists, only: %i[new create show destroy] do
     resources :bookmarks, only: %i[new create destroy]
   end
   resources :bookmarks, only: [:destroy]
